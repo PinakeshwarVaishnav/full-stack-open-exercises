b926d879a75c27fa20cd0237cf21765d8810a8f0
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2024-09-08 18:48:38 +0530

    Complete exercise 3.8

--- part3/index.js
+++ part3/index.js
@@ -3,6 +3,8 @@ const morgan = require('morgan')
 const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :response-time ms - Body::body'))
 
 let persons = [
     { 
