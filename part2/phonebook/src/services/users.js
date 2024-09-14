760aac47f316c01188b3c7afd4934d0b1c261202
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2024-09-14 18:47:39 +0530

    Complete exercise 3.9

--- part2/phonebook/src/services/users.js
+++ part2/phonebook/src/services/users.js
@@ -1,24 +1,25 @@
 import axios from "axios"
-const baseUrl = "http://localhost:3001/persons"
+const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
     return request.then((response) => {
         console.log("promise fulfilled");
         return response.data;
-      });
+    });
 }
 
 const create = newUser => {
     const request = axios.post(baseUrl, newUser)
     return request.then(response => {
         console.log("User added:", response.data);
-    return response.data;})
-    .catch((error) => {
-        console.log("Error adding user:", error);
-      });
+        return response.data;
+    })
+        .catch((error) => {
+            console.log("Error adding user:", error);
+        });
 
-    };
+};
 
 
 export default {
