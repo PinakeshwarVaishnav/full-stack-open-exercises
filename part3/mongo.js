45690e35a7ce1f99a51268ca6d0691565e6444fa
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2024-09-20 18:56:09 +0530

    Remove comments

--- part3/mongo.js
+++ part3/mongo.js
@@ -1,27 +1,28 @@
 const mongoose = require('mongoose');
 
-if (process.argv.length < 3) {
-  console.log('give password as argument')
+if (process.argv.length < 5) {
+  console.log('give password, name and number as argument')
   process.exit(1)
 }
 
 const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
 
 const url = `mongodb+srv://pinakeshwarvtech:${password}@cluster0.ebubh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery', false)
 
-mongoose.connect(url)
+mongoose.connect(url).then(() => console.log('mongodb connected'))
 
 const personSchema = new mongoose.Schema({
   name: String,
-  Number: Number
+  number: Number
 })
 
 const Person = mongoose.model('Person', personSchema)
 
-const person = // code for creating new phonebook person entry by taking the arguments from the command line while entering the command 'node mongo.js your_password name number' and then you have to take that input and put it in the database as well as do something about id, maybe generate it using a function of math.random or something
-
+const person =
   person.save().then(result => {
     console.log()
     mongoose.connection.close()
