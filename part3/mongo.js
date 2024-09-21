7ee1118c27bafd1265d1eac149ae2b8403383514
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2024-09-21 13:45:45 +0530

    Add functionality for displaying all the existing entries

--- part3/mongo.js
+++ part3/mongo.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
 
-if (process.argv.length < 5) {
-  console.log('give password, name and number as argument')
-  process.exit(1)
-}
-
 const password = process.argv[2]
 const name = process.argv[3]
 const number = process.argv[4]
@@ -26,11 +21,29 @@ const person = new Person({
   name: name,
   number: number
 })
-person.save().then(result => {
-  console.log(`added ${name} number ${number} to phonebook`)
-  mongoose.connection.close()
-}).catch(err => {
-  console.error(`error in saving ${person} to the database`)
-  mongoose.connection.close().then(() => console.log('connection closed'))
-    .catch(closeErr => console.error('Error closing connection :', closeErr))
-})
+
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person)
+    })
+    mongoose.connection.close()
+  })
+    .catch(err => console.error('error in person.find ', err))
+}
+
+else if (process.argv.length < 5) {
+  console.log('give password, name and number as argument')
+  process.exit(1)
+}
+
+else if (process.argv.length === 5) {
+  person.save().then(result => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  }).catch(err => {
+    console.error(`error in saving ${person} to the database`)
+    mongoose.connection.close().then(() => console.log('connection closed'))
+      .catch(closeErr => console.error('Error closing connection :', closeErr))
+  })
+}
