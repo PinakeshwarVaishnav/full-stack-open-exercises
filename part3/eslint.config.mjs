e69525815bfa89a2bc7caf92fde3bdb96195c7da
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2024-09-26 17:33:03 +0530

    Complete exercise 3.22

--- part3/eslint.config.mjs
+++ part3/eslint.config.mjs
@@ -1,7 +1,21 @@
 import globals from "globals";
+import airbnbConfig from 'eslint-config-airbnb-base';
 
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.node,
+      }
+    },
+    rules: {
+      ...airbnbConfig.rules,
+      'no-console': 'off',
+      'import/prefer-default-export': 'off',
+    },
+  },
 ];
