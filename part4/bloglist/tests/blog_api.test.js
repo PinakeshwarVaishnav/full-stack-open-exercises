e1110036eb543d47f9d100072ca3bff49fee0bdd
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2024-10-05 18:33:51 +0530

    complete exercise 4.9

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,15 +1,48 @@
 const { test, after, beforeEach } = require('node:test')
 const Blog = require('../models/blog')
 const mongoose = require('mongoose')
+const helper = require('./test_helper')
 const supertest = require('supertest')
 const app = require('../app')
 const assert = require('node:assert')
 const api = supertest(app)
 
-test('all notes are returned', async () => {
-  const response = await api.get('/api/blogs')
+beforeEach(async () => {
+  console.log('Deleting existing blogs')
+  await Blog.deleteMany({})
 
-  assert.strictEqual(response.body.length, 2)
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('all blogs are returned as json', async () => {
+  const response = await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+test('blog posts unique identifier is named id', async () => {
+  const response = await api.get('/api/blogs')
+  console.log('response body', response.body)
+
+  assert.ok(response.body[0].id)
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'the test database post request works',
+    author: 'me',
+    url: '-',
+    likes: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
 })
 
 after(async () => {
