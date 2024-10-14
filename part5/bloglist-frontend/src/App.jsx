c7748281fe7405c5c5174c955b518a24d6751bab
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2024-10-14 18:41:51 +0530

    complete exercise 5.1 and 5.2

---
+++ part5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,125 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+import Notification from './components/Notification'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [newBlog, setNewBlog] = useState("")
+  const [username, setUsername] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  const handleLogout = () => {
+    localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+  }
+
+  const handleLogin = async (event) => {
+    console.log('login form submitted')
+    event.preventDefault()
+    console.log(username, typeof (username))
+    console.log(password, typeof (password))
+
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (error) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const blogs = await blogService.getAll()
+        if (blogs) {
+          setBlogs(blogs)
+        } else {
+          console.log('No blogs found')
+        }
+      } catch (error) {
+        console.error('Error fetching blogs'.error)
+      }
+    }
+    fetchBlogs()
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON =
+      window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON && loggedUserJSON !== '' && loggedUserJSON !== undefined) {
+      const user = JSON.parse(loggedUserJSON)
+      console.log('Parsed user', user)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+              type='text'
+              value={username}
+              name='Username'
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type='password'
+              value={password}
+              name='Password'
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type='submit'>login</button>
+        </form>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      <Notification message={errorMessage} />
+      {user !== null &&
+        (
+          <div>
+            <p>{user.name} logged in </p>
+            <button onClick={handleLogout}>
+              Logout
+            </button>
+          </div>
+        )
+      }
+
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App
