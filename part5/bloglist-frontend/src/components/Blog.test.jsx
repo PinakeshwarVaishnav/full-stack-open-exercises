b4efa70de1fc890a0b016c8f942957f15e4c2fbc
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2024-10-21 18:17:15 +0530

    complete exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Blog from './Blog'
 import { expect } from "vitest";
 
@@ -12,9 +12,30 @@ test('renders blog title and author, but not URL or likes by default', () => {
 
   render(< Blog blog={blog} />)
 
+  screen.debug()
+
   expect(screen.getByText('testing')).toBeInTheDocument()
   expect(screen.getByText('developer')).toBeInTheDocument()
 
   expect(screen.queryByText('localhost')).not.toBeInTheDocument()
   expect(screen.queryByText('432')).not.toBeInTheDocument()
+})
+
+test('renders blog url and likes when the button view is clicked', () => {
+  const blog = {
+    title: 'testing',
+    author: 'developer',
+    url: 'localhost',
+    likes: 432,
+  }
+
+  render(< Blog blog={blog} />)
+
+
+  const button = screen.getByRole('button', { name: 'view' })
+  fireEvent.click(button)
+
+  expect(screen.queryByText('localhost')).toBeInTheDocument()
+  expect(screen.queryByText('432')).toBeInTheDocument()
+
 })
