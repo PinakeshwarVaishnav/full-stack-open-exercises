9cf3b1456ef263ee773d133fc58e046c84d45567
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2024-10-23 18:34:47 +0530

    add a log message for testing router

--- part4/bloglist/controllers/testing.js
+++ part4/bloglist/controllers/testing.js
@@ -5,6 +5,7 @@ const User = require('../models/user')
 router.post('/reset', async (request, response) => {
   await Blog.deleteMany({})
   await User.deleteMany({})
+  console.log('blogs and users deleted')
   response.status(204).end()
 })
 
