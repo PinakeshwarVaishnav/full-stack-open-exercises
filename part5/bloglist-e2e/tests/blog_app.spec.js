180875c588b38aa15606b53e411e3ae020e76254
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2024-10-23 16:53:09 +0530

    complete exercise 5.18

--- part5/bloglist-e2e/tests/blog_app.spec.js
+++ part5/bloglist-e2e/tests/blog_app.spec.js
@@ -1,12 +1,34 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith } = require('./helper')
+const { request } = require('http')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
     await page.goto('http://localhost:5173')
+    await request.post('/api/testing/reset')
+
+
+    await page.goto('')
   })
 
   test('Login form is shown', async ({ page }) => {
     const loginForm = await page.locator('form')
     await expect(loginForm).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'PGN', '123')
+
+      await expect(page.getByText('PGN logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'PGN', 'WRONG')
+
+      const errorDiv = await page.locator('.error')
+      await expect(errorDiv).toContainText('invalid username or password')
+      await expect(page.getByText('PGN logged in')).not.toBeVisible()
+    })
   })
 })
