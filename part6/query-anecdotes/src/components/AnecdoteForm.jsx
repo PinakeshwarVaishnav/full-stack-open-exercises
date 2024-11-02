eca04ef735da65c923f3883dc6089a432b0aa691
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2024-11-02 17:00:52 +0530

    complete exercise 6.24

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -9,13 +9,23 @@ const AnecdoteForm = () => {
     mutationFn: createAnecdote,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    },
+    onError: () => {
+      showNotification('too short anecdote, must have length 5 or more')
     }
   })
 
   const onCreate = (event) => {
     event.preventDefault()
+
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
+
+    if (content.length < 5) {
+      showNotification('too short anecdote, must have length 5 or more')
+      return
+    }
+
     newAnecdoteMutation.mutate({ content, votes: 0 })
     console.log('new anecdote added is', content)
     showNotification(`You added '${content}'`)
