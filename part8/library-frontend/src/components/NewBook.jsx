c9fe009a65af956c22afcf2b6080444b1525aa4b
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2024-11-16 19:35:48 +0530

    complete exercise 8.10

--- part8/library-frontend/src/components/NewBook.jsx
+++ part8/library-frontend/src/components/NewBook.jsx
@@ -1,11 +1,26 @@
+import { useMutation } from '@apollo/client'
 import { useState } from 'react'
+import ADD_BOOK from '../graphql/mutations/addBook.mutation'
+import GET_BOOKS from '../graphql/queries/GetBooks.query'
 
 const NewBook = (props) => {
+  const [addBook] = useMutation(ADD_BOOK, {
+    update(cache, { data: { addBook } }) {
+      const books = cache.readQuery({ query: GET_BOOKS })
+      console.log('books before updating', books)
+
+      cache.writeQuery({
+        query: GET_BOOKS,
+        data: { allBooks: books.allBooks.concat([addBook]) }
+      })
+    }
+  })
   const [title, setTitle] = useState('')
   const [author, setAuthor] = useState('')
   const [published, setPublished] = useState('')
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
+  const publishedAsNumber = parseInt(published)
 
   if (!props.show) {
     return null
@@ -15,6 +30,9 @@ const NewBook = (props) => {
     event.preventDefault()
 
     console.log('add book...')
+    addBook({
+      variables: { title, published: publishedAsNumber, author, genres }
+    })
 
     setTitle('')
     setPublished('')
