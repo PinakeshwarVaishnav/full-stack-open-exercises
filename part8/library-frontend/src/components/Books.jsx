e62445c3da38e65c9b5770fbbeb867f5b1c33e44
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2024-11-23 14:03:18 +0530

    complete exercise 8.19

--- part8/library-frontend/src/components/Books.jsx
+++ part8/library-frontend/src/components/Books.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import GET_BOOKS from '../graphql/queries/GetBooks.query'
 import { useQuery } from "@apollo/client"
 
 const Books = () => {
   const { loading, error, data } = useQuery(GET_BOOKS)
-  const [filteredBooks, setFilteredBooks] = useState(null)
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    if (data && Array.isArray(data.allBooks)) {
+      setBooks(data.allBooks)
+    }
+  }, [data])
+
+  useEffect(() => {
+    console.log('books value', books)
+  })
 
 
   if (loading) return
@@ -14,36 +24,50 @@ const Books = () => {
   <p>error: {error}</p>
 
   console.log('fetched data is', data)
-  const books = [data.allBooks]
-  console.log('books', books)
-  const genres = books[0].flatMap(book => book.genres)
+
+  const genres = data.allBooks.flatMap(book => book.genres)
   const uniqueGenres = [...new Set(genres)]
+  console.log('unique genres array', uniqueGenres)
+
+  const filterGenre = (genre) => {
+    const filteredBooks = books.filter(book => book.genres.includes(genre))
+    setBooks(filteredBooks)
+    console.log('filtered books by genre', genre)
+  }
+
+  const allGenres = () => {
+    setBooks(data.allBooks)
+    console.log('reset the genre filter')
+  }
 
 
   return (
-    <div>
+    <div className='relative overflow-x-auto shadow-md sm:rounded-lg'>
       <h2 className="text-3xl font-bold p-3">books</h2>
 
-      <table>
-        <tbody>
-          <tr>
+      <table className='w-full text-left text-gray-500'>
+        <thead className='text-s text-gray-700 '>
+          <tr className='bg-white border-b hover:bg-gray-100'>
             <th></th>
-            <th>author</th>
-            <th>published</th>
+            <th className='px-6 py-4'>author</th>
+            <th className='px-6 py-4'>published</th>
           </tr>
-          {books[0].map(book => (
-            <tr key={book.title}>
-              <td>{book.title}</td>
-              <td>{book.author.name}</td>
-              <td>{book.published}</td>
-            </tr>
-          ))}
+        </thead>
+        <tbody>
+          {books.length > 0 && (
+            books.map(book => (
+              <tr className='bg-white border-b hover:bg-gray-100' key={book.title}>
+                <td>{book.title}</td>
+                <td>{book.author.name}</td>
+                <td>{book.published}</td>
+              </tr>
+            )))}
         </tbody>
       </table>
-      {uniqueGenres.map(genre => (
-        <button className='bg-blue-500 hover:bg-blue-950 text-white font-bold p-1 px-2 rounded'>{genre}</button>
+      {uniqueGenres.map((genre, index) => (
+        <button onClick={() => filterGenre(genre)} key={index} className='bg-blue-500 hover:bg-blue-950 text-white font-bold p-1 px-2 m-1 rounded'>{genre}</button>
       ))}
-      <button className='bg-blue-500 hover:bg-blue-950 text-white font-bold p-1 px-2 rounded'>all genres</button>
+      <button onClick={allGenres} className='bg-blue-500 hover:bg-blue-950 text-white font-bold p-1 px-2 rounded'>all genres</button>
     </div >
   )
 }
