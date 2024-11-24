12a4c5d333c894546df91dbf7bb01f4a89e423d4
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2024-11-24 18:22:55 +0530

    complete exercise 8.25

--- part8/library-frontend/src/components/Notification.jsx
+++ part8/library-frontend/src/components/Notification.jsx
@@ -1,16 +1,25 @@
 import { useEffect } from 'react'
-import { useSubscription } from '@apollo/client'
+import { useApolloClient, useSubscription } from '@apollo/client'
 import BOOK_ADDED from '../graphql/subscriptions/subscription'
+import GET_BOOKS from '../graphql/queries/GetBooks.query'
 
 const Notification = () => {
+  const client = useApolloClient()
   const { data, loading, error } = useSubscription(BOOK_ADDED)
 
   useEffect(() => {
     if (data && data.bookAdded) {
       const { title, author } = data.bookAdded
       window.alert(`new book  ${title} by ${author.name} added`)
+
+      client.cache.updateQuery({ query: GET_BOOKS }, (existingData) => {
+        const allBooks = existingData ? existingData.allBooks : []
+        return {
+          allBooks: allBooks.concat(data.bookAdded)
+        }
+      })
     }
-  })
+  }, [data, client])
 
   if (loading) return
   if (error) return <p>error for subscription: {error.message}</p>
